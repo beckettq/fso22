7db49f32202a8d4fc7d570d270c65be858303271
beckettq <beckettebquirk@gmail.com>
2022-07-09 14:41:28 -0400

    created anecdotes app

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+
+const newSelection = (n) => {
+  let x = Math.floor(Math.random() * n)
+  return x
+}
+
+const maxIndex = (arr) => {
+  let max = 0
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[max]) {max = i}
+  }
+  return max
+}
+
+const Anecdote = (props) => {
+  return (
+  <div>
+    {props.anecdotes[props.index]}
+    <div>
+      {`has ${props.points[props.index]} votes`}
+    </div>
+
+  </div>
+  )
+}
+
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, updatePoints] = useState([0, 0, 0, 0, 0, 0, 0])
+
+  const setnew = () => setSelected(newSelection(anecdotes.length))
+  const vote = (index) => {
+    const copy = [...points]
+    copy[index] += 1
+    updatePoints(copy)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdotes = {anecdotes} index = {selected} points = {points}></Anecdote>
+      <div>
+        <button onClick = {setnew}>next anecdote</button>
+        <button onClick = {() => vote(selected)}>vote</button>
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdotes = {anecdotes} index = {maxIndex(points)} points = {points}></Anecdote>
+    </div>
+    
+  )
+}
+
+export default App
